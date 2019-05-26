9c6a9b2fe6c1942159ab9af0f17000d68f6af1c7
Tiago Sá Marques <samarques.tiago@gmail.com>
2019-05-26 22:56:14 +0100

    ctb: Fix includes

--- Inc/ctb.h
+++ Inc/ctb.h
@@ -1,8 +1,6 @@
 #ifndef __CTB_H__
 #define __CTB_H__
 
-#include "stm32f7xx_hal.h"
-
 int
 ctb_main(void);
 
--- Src/ctb.c
+++ Src/ctb.c
@@ -1,10 +1,14 @@
 #include "ctb.h"
 
-#include "imc-c/Acceleration.h"
-#include "imc-c/EstimatedState.h"
-#include "imc-c/LoggingControl.h"
-#include "imc-c/Heartbeat.h"
-#include "imc-c/GpsFix.h"
+//! HAL inclues
+#include "stm32f7xx_hal.h"
+
+//! IMC includes
+#include <imc-c/Acceleration.h>
+#include <imc-c/EstimatedState.h>
+#include <imc-c/LoggingControl.h>
+#include <imc-c/Heartbeat.h>
+#include <imc-c/GpsFix.h>
 
 int
 ctb_main(void)
