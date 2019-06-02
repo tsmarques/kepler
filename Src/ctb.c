49fcaa38bf93f582bb200ab7616dd6882058742c
Tiago Sá Marques <samarques.tiago@gmail.com>
2019-06-02 10:16:07 +0100

    ctb: read IMU measurements and fill proper ICM messages

--- Src/ctb.c
+++ Src/ctb.c
@@ -5,6 +5,7 @@
 
 //! CTB
 #include <usart.h>
+#include <icm20948.h>
 
 //! IMC includes
 #include <imc-c/Acceleration.h>
@@ -12,14 +13,29 @@
 #include <imc-c/LoggingControl.h>
 #include <imc-c/Heartbeat.h>
 #include <imc-c/GpsFix.h>
+#include <imc-c/AngularVelocity.h>
+
+static const char* hello_msg = "Nosy bastard ain't ya?\n";
+
+static Acceleration imc_accel;
+static AngularVelocity imc_angular_vel;
 
 int
 ctb_main(void)
 {
-  const char* hello_msg = "Nosy bastard ain't ya?\n";
+  uart_print(hello_msg, strlen(hello_msg));
+
+  imc_accel = Acceleration_new();
+  imc_angular_vel = AngularVelocity_new();
+
   while(1)
   {
-    uart_print(hello_msg, strlen(hello_msg));
+    imu_get_accelerations(&imc_accel.x, &imc_accel.y, &imc_accel.z);
+    imu_get_angular_velocities(&imc_angular_vel.x, &imc_angular_vel.y, &imc_angular_vel.z);
+
+    // imc_accel.time = ...
+    // imc_angular_vel = ...
+
     HAL_Delay(1000);
   }
 }
