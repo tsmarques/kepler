0d19d7d0a98afae2c44fe90706994832a9b64c27
Tiago Sá Marques <samarques.tiago@gmail.com>
2019-09-22 10:38:39 +0100

    No need for sdmmc and dma for now

--- Src/main.c
+++ Src/main.c
@@ -99,8 +99,6 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_SDMMC1_SD_Init();
   MX_SPI2_Init();
   MX_UART7_Init();
   MX_UART8_Init();
