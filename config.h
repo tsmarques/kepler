9b4f99daf142c4482fb5b3af982149ca832a6944
Tiago Sá Marques <samarques.tiago@gmail.com>
2019-09-29 11:13:22 +0100

    Add preliminary configuration for another testboad (wip)

--- config.h
+++ config.h
@@ -4,6 +4,8 @@
 #if defined(STM32F767xx)
 #include "conf/stm32f767zi-discovery.h"
 
+#elif defined(STM32F767xx_TESTBOARD_1)
+#include "conf/stm32f767-testboard-1.h"
 
 #else
 #error "No valid configuration found"
