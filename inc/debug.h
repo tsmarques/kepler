d09ef0177e43e50af4243da4dd9da181a4e20448
Tiago Sá Marques <samarques.tiago@gmail.com>
2019-12-07 19:55:17 +0000

    inc/debug: Add compile-time debug information

--- inc/debug.h
+++ inc/debug.h
@@ -2,6 +2,7 @@
 #define KEPLER_DEBUG_H
 
 #if defined(ARM_SEMIHOST)
+#  warning "Compiling with ARM_SEMIHOST"
 #  include <stdio.h>
 #  define debug_init() initialise_monitor_handles()
 #  define trace(...) printf(__VA_ARGS__)
