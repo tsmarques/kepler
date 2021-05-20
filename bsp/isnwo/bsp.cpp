6039cac33bef469015bb1f4b50204370cf9ce5df
Tiago Sá Marques <samarques.tiago@gmail.com>
2021-05-20 10:32:31 +0100

    bsp: Main loop at each bsp file

--- bsp/isnwo/bsp.cpp
+++ bsp/isnwo/bsp.cpp
@@ -1,6 +1,12 @@
 #include "../trace.h"
 #include <Config.hpp>
 
+#include <data/Attitude.hpp>
+#include <data/BasicData.hpp>
+#include <data/GpsFix.hpp>
+#include <data/Pressure.hpp>
+#include <data/Temperature.hpp>
+#include <driver/icm20948/ICM20948.hpp>
 #include <driver/mpl3115a2/MPL3115A2.hpp>
 
 void
@@ -76,5 +82,43 @@ bsp_init(mailbox_t* data_bus)
   trace("mpl3115a2: starting driver\r\n");
   i2cStart(&I2CD2, &I2C2_CONFIG);
 
-  kepler::drivers::MPL3115A2::start(data_bus, &I2CD2);
+  trace("mpl3115a2: starting driver\r\n");
+//  kepler::drivers::MPL3115A2::start(data_bus, &I2CD2);
+  kepler::drivers::ICM20948::start(data_bus, &SPID2);
+
+  kepler::BasicData* msg;
+  while (!chThdShouldTerminateX())
+  {
+    msg_t ret = chMBFetchTimeout(data_bus, (msg_t *)&msg, chTimeMS2I(500));
+    if (ret == MSG_OK)
+    {
+      switch (msg->getType())
+      {
+        case kepler::DT_ATTITUDE:
+        {
+          kepler::data::Attitude* att = static_cast<kepler::data::Attitude*>(msg);
+          trace("Attitude | phi: %.3f theta: %.3f psi: %.3f\r\n",
+                att->m_phi, att->m_theta, att->m_psi);
+          break;
+        }
+        case kepler::DT_GPSFIX:
+        {
+          kepler::data::GpsFix* fix = static_cast<kepler::data::GpsFix*>(msg);
+          trace("GpsFix | lat: %.3f lon: %.3f h: %.3f\r\n",
+                fix->m_lat, fix->m_lon, fix->m_height);
+          break;
+        }
+        case kepler::DT_PRESSURE:
+        {
+          kepler::data::Pressure* p = static_cast<kepler::data::Pressure*>(msg);
+          trace("Pressure | value %.3f\r\n", p->value);
+          break;
+        }
+        case kepler::DT_TEMPERATURE:
+          kepler::data::Temperature* t = static_cast<kepler::data::Temperature*>(msg);
+          trace("Temperature | value %.3f\r\n", t->value);
+          break;
+      }
+    }
+  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -18,40 +18,4 @@ int main()
   chMBObjectInit(&data_bus, data_msg, 10);
 
   bsp_init(&data_bus);
-
-  kepler::BasicData* msg;
-  while (!chThdShouldTerminateX())
-  {
-    msg_t ret = chMBFetchTimeout(&data_bus, (msg_t *)&msg, chTimeMS2I(500));
-    if (ret == MSG_OK)
-    {
-      switch (msg->getType())
-      {
-        case kepler::DT_ATTITUDE:
-        {
-          kepler::data::Attitude* att = static_cast<kepler::data::Attitude*>(msg);
-          trace("Attitude | phi: %.3f theta: %.3f psi: %.3f\r\n",
-                att->m_phi, att->m_theta, att->m_psi);
-          break;
-        }
-        case kepler::DT_GPSFIX:
-        {
-          kepler::data::GpsFix* fix = static_cast<kepler::data::GpsFix*>(msg);
-          trace("GpsFix | lat: %.3f lon: %.3f h: %.3f\r\n",
-                fix->m_lat, fix->m_lon, fix->m_height);
-          break;
-        }
-        case kepler::DT_PRESSURE:
-        {
-          kepler::data::Pressure* p = static_cast<kepler::data::Pressure*>(msg);
-          trace("Pressure | value %.3f\r\n", p->value);
-          break;
-        }
-        case kepler::DT_TEMPERATURE:
-          kepler::data::Temperature* t = static_cast<kepler::data::Temperature*>(msg);
-          trace("Temperature | value %.3f\r\n", t->value);
-          break;
-      }
-    }
-  }
 }
